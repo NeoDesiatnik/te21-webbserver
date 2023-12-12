1dfd33e1d269e5f760969442953ac9cc16954d1c
NeoDesiatnik <neobackstrom.desiatnik@elev.ga.ntig.se>
2023-12-12 08:54:11 +0100

    text change

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', function (req, res) {
-  res.render('index.njk', { title: 'Hello from template' })
+  res.render('index.njk', { title: 'Welcome to Los Pollos Hermanos' })
 })
 
 module.exports = router
