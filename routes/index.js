b9e3c5bfedf259140ecbab78cedf0a78144f7b8f
NeoDesiatnik <neobackstrom.desiatnik@elev.ga.ntig.se>
2023-12-12 11:15:23 +0100

    Los Pollos Hermanos

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,15 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', function (req, res) {
-  res.render('index.njk', { title: 'Welcome to Los Pollos Hermanos' })
+  res.render('index.njk', { title: 'Los Pollos Hermanos' })
+})
+
+router.get('/about', function (req, res) {
+  res.render('about.njk', { title: 'About Los Pollos Hermanos' })
+})
+
+router.get('/menu', function (req, res) {
+  res.render('menu.njk', { title: 'Los Pollos Hermanos Menu' })
 })
 
 module.exports = router
